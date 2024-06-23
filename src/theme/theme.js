2721ddefb309bed76430f80db65c59ff58116880
oErco <erickrsilva@hotmail.com>
2024-06-23 15:52:25 -0300

    Feat: theme provider

---
+++ src/theme/theme.js
@@ -0,0 +1,61 @@
+import { createTheme } from "@mui/material";
+
+const palette = {
+    primary: {
+        light: '#FFFFFF',
+        main: '#FAFAFA',
+        dark: '#EAEAEA'
+    },
+    secondary: {
+        light: '#7F8586',
+        main: '#595959',
+        dark: '#34393a'
+    },
+    success: {
+        light: '#81c784',
+        main: '#4caf50',
+        dark: '#388e3c'
+    },
+    error: {
+        light: '#e57373',
+        main: '#f44336',
+        dark: '#d32f2f'
+    },
+    warning: {
+        light: '#ffb74d',
+        main: '#ff9800',
+        dark: '#f57c00'
+    },
+    info: {
+        light: '#64b5f6',
+        main: '#2196f3',
+        dark: '#1976d2'
+    },
+}
+
+export const theme = createTheme({
+    palette: palette,
+    components: {
+        MuiDataGrid: {
+            styleOverrides: {
+                root: {
+                    border: "none",
+                    color: palette.secondary.light,
+                },
+                columnHeader: {
+                    fontSize: "0.75rem",
+                    textAlign: "center",
+                },
+                row: {
+                    margin: "0.25rem",
+                    padding: "0.25rem",
+                },
+            },
+        },
+    },
+    mixins: {
+        MuiDataGrid: {
+            containerBackground: "rgba(0, 0, 0, 0)",
+        },
+    },
+});
